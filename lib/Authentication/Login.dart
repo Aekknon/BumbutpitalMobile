e77134c30d7742742493208aa151403f379f8825
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-08 01:00:58 +0700

    Add Sign in Page
    Email,Password,remember me

--- lib/Authentication/Login.dart
+++ lib/Authentication/Login.dart
@@ -1,57 +1,133 @@
-import 'package:bumbutpital/Authentication/SignUp.dart';
-import 'package:bumbutpital/MainPage.dart';
+// import 'package:bumbutpital/Authentication/SignUp.dart';
+
+// import 'package:bumbutpital/SplashScreen.dart';
+// import 'package:bumbutpital/mainPage.dart';
+// import 'package:bumbutpital/SplashScreen.dart';
+import 'package:bumbutpital/mainPage.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  var isPasswordHidden = true;
+  var isRemember = true;
+
+  void onSubmit() {
+    // access controller value buy using _email.text
+    // bla bla
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "LoginPage",
       home: Scaffold(
-        appBar: AppBar(title: Text("LoginPage")),
-        body: const ButtonWidget(),
+        backgroundColor: Color(0XFFECF2FF),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 84),
+                  Container(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(height: 24),
+                  _textField(
+                    _email,
+                    "Email",
+                    Icon(Icons.email),
+                  ),
+                  SizedBox(height: 10),
+                  _textField(
+                    _password,
+                    "Password",
+                    Icon(Icons.password),
+                  ),
+                  Container(
+                    child: Text(
+                      'Forget Password?',
+                      style: TextStyle(height: 2),
+                    ),
+                    alignment: Alignment.bottomRight,
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: isRemember,
+                          onChanged: (bool? value) {
+                            if (value != null) {
+                              setState(() {
+                                isRemember = value;
+                              });
+                            }
+                          },
+                        ),
+                        Text('Remember Me'),
+                      ],
+                    ),
+                  ),
+                  Container(),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => MainPage()));
+                    },
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
-}
 
-class ButtonWidget extends StatefulWidget {
-  const ButtonWidget({Key? key}) : super(key: key);
-
-  @override
-  State<ButtonWidget> createState() => _ButtonWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _ButtonWidgetState extends State<ButtonWidget> {
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style =
-        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ElevatedButton(
-            style: style,
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MainPage()));
-            },
-            child: const Text('MainPage'),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: style,
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => SignUp()));
-            },
-            child: const Text('SignUp'),
-          ),
-        ],
+  TextField _textField(TextEditingController cont, String label, Icon icon) {
+    return TextField(
+      controller: cont,
+      obscureText: label == "Password" ? isPasswordHidden : false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: label,
+        hintText: "Enter your ${label.toLowerCase()}",
+        prefixIcon: icon,
+        suffixIcon: label == "Password"
+            ? InkWell(
+                onTap: () {
+                  setState(() {
+                    isPasswordHidden = !isPasswordHidden;
+                  });
+                },
+                child: isPasswordHidden
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.visibility),
+              )
+            : null,
+        contentPadding: EdgeInsets.symmetric(vertical: 0),
       ),
+      keyboardType: TextInputType.visiblePassword,
+      autocorrect: true,
     );
   }
 }
