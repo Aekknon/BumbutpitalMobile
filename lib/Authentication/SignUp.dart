7404c93e61863502b9460561ef27411d05bdb22c
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-08 21:22:14 +0700

    Add input box of sign up Screen

--- lib/Authentication/SignUp.dart
+++ lib/Authentication/SignUp.dart
@@ -1,21 +1,109 @@
+import 'package:bumbutpital/Authentication/SignUp.dart';
 import 'package:bumbutpital/Questionare/MainQuestion.dart';
+import 'package:bumbutpital/mainPage.dart';
 import 'package:flutter/material.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   @override
-  Widget build(BuildContext contxt) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register Page"),
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUp> {
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final _FullName = TextEditingController();
+  final _SurName = TextEditingController();
+  var isPasswordHidden = true;
+  var isRemember = true;
+
+  void onSubmit() {
+    // access controller value by using _email.text
+    // bla bla
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Sign Up Page",
+        home: Scaffold(
+            backgroundColor: Color(0XFFECF2FF),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 72),
+                        Container(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                          alignment: Alignment.center,
+                        ),
+                        SizedBox(height: 39),
+                        _textField(
+                          _FullName,
+                          "Full Name",
+                        ),
+                        SizedBox(height: 39),
+                        _textField(
+                          _SurName,
+                          "Surname",
+                        ),
+                        SizedBox(height: 39),
+                        _textField(
+                          _email,
+                          "Email",
+                        ),
+                        SizedBox(height: 39),
+                        _textField(
+                          _password,
+                          "Password",
+                        ),
+                        SizedBox(height: 39),
+                        _textField(
+                          _password,
+                          "Confirm Password",
+                        ),
+                        SizedBox(height: 29),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainQuestion()));
+                          },
+                          style: ElevatedButton.styleFrom(
+                              primary: Color(0XFFFE7940),
+                              fixedSize: Size(350, 50)),
+                          child: Text("REGISTER"),
+                        ),
+                      ]),
+                ),
+              ),
+            )));
+  }
+
+  TextField _textField(TextEditingController cont, String label) {
+    return TextField(
+      controller: cont,
+      obscureText: label == "Password" ? isPasswordHidden : false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: label,
+        hintText: "Enter your ${label.toLowerCase()}",
+        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
       ),
-      body: Center(
-        child: ElevatedButton(
-            child: Text("Go to Questionare"),
-            onPressed: () {
-              Navigator.push(contxt,
-                  MaterialPageRoute(builder: (context) => MainQuestion()));
-            }),
-      ),
+      keyboardType: TextInputType.visiblePassword,
+      autocorrect: true,
     );
   }
 }
