d5b0c86c999abfc317d6af3ba6d2a0d190b0c555
Bew <wisa.gubewgibi@mail.kmutt.ac.th>
2021-09-10 18:40:09 +0700

    [Add] QR Code Page

--- lib/HospitalPage/QRcodePage.dart
+++ lib/HospitalPage/QRcodePage.dart
@@ -1,21 +1,103 @@
 import 'package:bumbutpital/MainPage.dart';
 import 'package:flutter/material.dart';
 
-class QRcodePage extends StatelessWidget {
+class QRCodePage extends StatefulWidget {
+  QRCodePage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext contxt) {
+  _QRCodePageState createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("QR Code Page"),
+        iconTheme: IconThemeData(color: Colors.white),
+        centerTitle: true,
+        backgroundColor: Color(hexColor('#6367EA')),
+        elevation: 0,
       ),
-      body: Center(
-        child: ElevatedButton(
-            child: Text("Go to MainPage"),
-            onPressed: () {
-              Navigator.push(
-                  contxt, MaterialPageRoute(builder: (context) => MainPage()));
-            }),
+      body: Container(
+        color: Color(hexColor('#6367EA')),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'QR CODE',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://th.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"),
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Please Scan',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.31,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Color(hexColor('#FE7940')),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                child: Text(
+                  'COMPLETE',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      decoration: TextDecoration.none),
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MainPage()));
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+int hexColor(String color) {
+  String newColor = '0xff' + color;
+
+  newColor = newColor.replaceAll('#', '');
+  int finalColor = int.parse(newColor);
+  return finalColor;
 }
