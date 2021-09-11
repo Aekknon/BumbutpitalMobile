2d78b7953878f0576e71d727332678be359247ab
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-12 00:03:29 +0700

    [ADD] Component Bottom Navigation bar

---
+++ lib/ComponentMainScreen/bottomnavbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class bottomnavbar extends StatefulWidget {
+  bottomnavbar({Key? key}) : super(key: key);
+
+  @override
+  _bottomnavbarState createState() => _bottomnavbarState();
+}
+
+class _bottomnavbarState extends State<bottomnavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
