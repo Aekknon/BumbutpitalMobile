4df96a191abb3643098917377ef6a28d4c7f73f5
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-12 18:33:23 +0700

    [Edit] Route main Screen

--- lib/HospitalPage/QRcodePage.dart
+++ lib/HospitalPage/QRcodePage.dart
@@ -18,75 +18,77 @@ class _QRCodePageState extends State<QRCodePage> {
         backgroundColor: Color(hexColor('#6367EA')),
         elevation: 0,
       ),
-      body: Container(
-        color: Color(hexColor('#6367EA')),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                'QR CODE',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    decoration: TextDecoration.none,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          "https://th.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"),
-                      fit: BoxFit.cover,
-                    )),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Please Scan',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    decoration: TextDecoration.none,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.31,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Color(hexColor('#FE7940')),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                    textStyle:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                child: Text(
-                  'COMPLETE',
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(hexColor('#6367EA')),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'QR CODE',
                   style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 24,
                       color: Colors.white,
-                      decoration: TextDecoration.none),
+                      decoration: TextDecoration.none,
+                      fontWeight: FontWeight.bold),
                 ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MainScreen()));
-                },
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://th.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"),
+                        fit: BoxFit.cover,
+                      )),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Please Scan',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.31,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(hexColor('#FE7940')),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                      textStyle:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  child: Text(
+                    'COMPLETE',
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        decoration: TextDecoration.none),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MainScreen()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
