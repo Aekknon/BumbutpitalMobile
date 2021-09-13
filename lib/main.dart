adae2e0eabfaf9c7e98579bd70a92eb7121f81b4
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-13 19:51:24 +0700

    [ADD] Route Show result Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Questionare/ShowResult.dart';
 import 'SplashScreen.dart';
 
 void main() {
@@ -9,8 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         //use MaterialApp() widget like this
-        home: SplashScreen() //create new widget class for this 'home' to
+        home: SplashScreen(), //create new widget class for this 'home' to
+        routes: {
+          ShowResult.routeName: (_) => ShowResult(),
+        }
         // escape 'No MediaQuery widget found' error
         );
   }
