e5c853597ed438a2f78a6c10e2b8ec1a4f42823c
Bew <wisa.gubewgibi@mail.kmutt.ac.th>
2021-09-14 16:06:36 +0700

    [Add]ContentCard and [Fix]MainContent

---
+++ lib/widgets/ContentCard.dart
@@ -0,0 +1,26 @@
+import 'package:bumbutpital/ContentPage/ContentDetail.dart';
+import 'package:flutter/material.dart';
+
+class ContentCard extends StatelessWidget {
+  const ContentCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => ContentDetail()));
+      },
+      child: Container(
+          height: 250,
+          width: 350,
+          child: Image.network(
+              'https://allwellhealthcare.com/wp-content/uploads/2020/12/depression-disorder.jpg')),
+    ),
+    SizedBox(height: 20,)
+      ],
+    );
+  }
+}
