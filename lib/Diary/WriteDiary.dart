698300ebafa1fab036977e74671aa86889223e10
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-09-15 22:39:33 +0700

    [Fix] fix bug write diary screen

--- lib/Diary/WriteDiary.dart
+++ lib/Diary/WriteDiary.dart
@@ -13,159 +13,178 @@ class _WriteDiaryState extends State<WriteDiary> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        
-        
-        backgroundColor: Color(hexColor('#ECF2FF')),
-        elevation: 0,
-      ),
-      body: Container(
-         color: Color(hexColor('#ECF2FF')),
-        height: MediaQuery.of(context).size.height,
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Container(
-              child: Column(children: [
-                Container(
-                  color: Color(hexColor('#ECF2FF')),
-                  height: MediaQuery.of(context).size.height*0.2,
-                  child:   Container(
-                  padding: const EdgeInsets.only(
-                      top: 10, bottom: 10, left: 20, right: 20),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(width: 15,),
-                         Column(
-                           children: [
-                              Text('Monday',
-                        style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.bold),),
-                      Text('May 20',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            ),),
-                           ],
-                         ),
-                            Spacer()
-                        ],
-                      ),
-                      SizedBox(height: 25,),
-                      Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          'Title: ',
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                              decoration: TextDecoration.none,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          style: TextStyle(color: Color(hexColor('#6367EA'))),
-                          decoration: InputDecoration(
-                            labelText: '    Enter Title Questions',
-                            fillColor: Colors.white,
-                            contentPadding: EdgeInsets.symmetric(vertical: 00),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                    ],
-                  )),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height*0.7,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(70),
-                        topLeft: Radius.circular(70)),
-                        
-                  ),
-                  child: Expanded(
-                    child: SingleChildScrollView(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Color(hexColor('#ECF2FF')),
+          elevation: 0,
+        ),
+        body: Container(
+          color: Color(hexColor('#ECF2FF')),
+          height: MediaQuery.of(context).size.height,
+          child: Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      color: Color(hexColor('#ECF2FF')),
+                      height: MediaQuery.of(context).size.height * 0.25,
                       child: Container(
-                        
-                       padding:
-                const EdgeInsets.only(top: 34, bottom: 10, left: 30, right: 30),
-                child: Container(
-                  padding: const EdgeInsets.only(
-                      top: 10, bottom: 10, left: 20, right: 20),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 15,
+                          padding: const EdgeInsets.only(
+                              top: 10, bottom: 10, left: 20, right: 20),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        'Monday',
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            color: Colors.black,
+                                            decoration: TextDecoration.none,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'May 20',
+                                        // textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                          decoration: TextDecoration.none,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  // Spacer()
+                                ],
+                              ),
+                              SizedBox(
+                                height: 25,
+                              ),
+                              Row(
+                                children: [
+                                  // Container(
+                                  //   child: Text(
+                                  //     'Title: ',
+                                  //     style: TextStyle(
+                                  //         fontSize: 20,
+                                  //         color: Colors.black,
+                                  //         decoration: TextDecoration.none,
+                                  //         fontWeight: FontWeight.bold),
+                                  //   ),
+                                  // ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.9,
+                                    child: TextField(
+                                      style: TextStyle(
+                                          color: Color(hexColor('#6367EA'))),
+                                      decoration: InputDecoration(
+                                        hintStyle: TextStyle(fontSize: 24),
+                                        hintText: 'Enter Title Diary',
+                                        fillColor: Colors.white,
+                                        // contentPadding: EdgeInsets.symmetric(
+                                        //     vertical: 00, horizontal: 0),
+                                        border: UnderlineInputBorder(),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          )),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(70),
+                            topLeft: Radius.circular(70)),
                       ),
-                      Text(
-                        'Question: ',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          style: TextStyle(color: Color(hexColor('#6367EA'))),
-                          decoration: InputDecoration(
-                            
-                            fillColor: Colors.white,
-                            contentPadding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.2),
-                            border: OutlineInputBorder(),
+                      child: Expanded(
+                        child: SingleChildScrollView(
+                          child: Container(
+                            padding: const EdgeInsets.only(
+                                top: 10, bottom: 10, left: 30, right: 30),
+                            child: Container(
+                                padding: const EdgeInsets.only(
+                                    top: 10, bottom: 10, left: 20, right: 20),
+                                child: Column(
+                                  children: [
+                                    // SizedBox(
+                                    //   height: 15,
+                                    // ),
+                                    // Text(
+                                    //   'Your story ',
+                                    //   style: TextStyle(
+                                    //       fontSize: 16,
+                                    //       color: Colors.black,
+                                    //       decoration: TextDecoration.none,
+                                    //       fontWeight: FontWeight.bold),
+                                    // ),
+                                    // SizedBox(
+                                    //     // height: 10,
+                                    //     ),
+                                    Container(
+                                      child: TextField(
+                                        maxLines: 15,
+                                        // textAlign: TextAlign.start,
+                                        style: TextStyle(
+                                            color: Color(hexColor('#6367EA'))),
+                                        decoration: InputDecoration(
+                                          // labelStyle: St,
+                                          hintText: 'Enter your story here',
+                                          contentPadding: EdgeInsets.symmetric(
+                                              vertical: 24, horizontal: 8),
+                                          border: InputBorder.none,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 30,
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Color(hexColor('#FE7940')),
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 100, vertical: 15),
+                                          textStyle: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold)),
+                                      child: Text(
+                                        'Add Diary',
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.white,
+                                            decoration: TextDecoration.none),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    MainDiary()));
+                                      },
+                                    ),
+                                  ],
+                                )),
                           ),
                         ),
                       ),
-                      SizedBox(height: 30,),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Color(hexColor('#FE7940')),
-                            padding:
-                                EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                            textStyle: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                        child: Text(
-                          'Add Diary',
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                              decoration: TextDecoration.none),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>MainDiary()));
-                        },
-                      ),
-                    ],
-                  )),
-                      ),
-                    ),),
+                    ),
+                  ],
                 ),
-                
-              ],),
-            ),),),
-      )
-    );
-
+              ),
+            ),
+          ),
+        ));
   }
 }
 
